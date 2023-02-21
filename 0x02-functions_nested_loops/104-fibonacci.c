7ac62845a493ac13a72396e6e0d027f766907d48
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-02-21 21:15:10 +0200

    .

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int counter = 2;
 
-	double a = 1;
-	double b = a + 1;
-	double c = a + b;
+	float a = 1;
+	float b = a + 1;
+	float c = a + b;
 
 	printf("%.0f, ", a);
 	printf("%.0f, ", b);
-	while (counter <= 98)
+	while (counter < 98)
 	{
 		counter++;
 		printf("%.0f", c);
