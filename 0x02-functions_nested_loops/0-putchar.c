68759faceca780aca7573501c819485cb3ded463
MennaAnwar <menna_m20@yahoo.com>
2023-02-21 07:26:02 -0800

    put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *text = "_putchar";
+
+	while (*text)
+	{
+		_putchar(*text);
+		text++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
