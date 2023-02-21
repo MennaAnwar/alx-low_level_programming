71164b41bc2bf73dca232508478f8e2bde3f60de
MennaAnwar <menna_m20@yahoo.com>
2023-02-21 07:55:50 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	n = 'a';
+	while (n <= 'z')
+	{
+		_putchar(n++);
+	}
+	_putchar('\n');
+}
