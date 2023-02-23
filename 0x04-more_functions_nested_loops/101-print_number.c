3c15b83b289a1cf9da8b354358d33f9450e8e3c1
MennaAnwar <menna_m20@yahoo.com>
2023-02-23 06:38:15 -0800

    .

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,21 @@
 #include "main.h"
 
 /**
- * print_number - prints number
- *
- * @n: integer to print to character
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
 void print_number(int n)
 {
-	int d;
-
-	if (n == 0)
-	{
-		_putchar('0');
-	}
 	if (n < 0)
 	{
-		_putchar('-');
 		n = -n;
+		_putchar('-');
 	}
-	while (n != 0)
+
+	if (n / 10)
 	{
-		d = n % 10;
-		n /= 10;
-		_putchar('0' + d);
+		print_number(n / 10);
 	}
+
+	_putchar((n % 10) + '0');
 }
