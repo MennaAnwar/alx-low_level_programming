0dc6c5d19f03ea7125686cc636f3934a6d9422ca
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-02-27 17:31:05 +0200

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,17 +9,14 @@ void rev_string(char *s)
 	int i;
 	int j;
 	int temp;
-	int length;
 
 	while (s[i] != 0)
 	{
 		i++;
 	}
 
-	length = i;
+	j = i - 1;
 	i = 0;
-	j = length - 1;
-
 	while (i < j)
 	{
 		temp = s[i];
