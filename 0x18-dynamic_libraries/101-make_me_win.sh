5fbe54426bf36862cb9a82586dd5d7f6433737c9
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-05-22 19:39:07 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/MennaAnwar/alx-low_level_programming/tree/main/0x18-dynamic_libraries/makemewin.so
+wget -P ../ https://github.com/MennaAnwar/alx-low_level_programming/blob/main/0x18-dynamic_libraries/makemewin.so
 export LD_PRELOAD=../makemewin.so
