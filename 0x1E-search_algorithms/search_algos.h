dafb8445233947c0d67d9c6837f9cdec66e038f0
MennaAnwar <menna_m20@yahoo.com>
2023-09-13 11:33:21 +0300

    exponential search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,7 @@ int binary_search(int *array, size_t size, int value);
 int recursive_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int _binary_search(int *array, size_t left, size_t right, int value);
 
 #endif
